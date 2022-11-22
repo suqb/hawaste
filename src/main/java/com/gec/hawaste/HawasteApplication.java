a8d1ec42a4309892fea8c2c50a27daca74228a71
suqb1 <2907829008@qq.com>
2022-11-22 14:10:02 +0800

    添加代码生成器的代码

---
+++ src/main/java/com/gec/hawaste/HawasteApplication.java
@@ -0,0 +1,13 @@
+package com.gec.hawaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawasteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hawaste/hawaste/HawasteApplicationTests.java
@@ -0,0 +1,13 @@
+package hawaste.hawaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
